9fe53025ad3d71383ef3f39ea338942de26600ce
TBriggz <skaterbriggz@gmail.com>
2020-02-13 21:36:48 -0500

    Updated for accurate starting ground.

---
+++ SlotMachine-Tristen_Briggs/Assets/Scripts/PlayerHealthScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Enemy")
+        {
+            GameControl.health -= 1;
+        }
+
+    }
+
+}
